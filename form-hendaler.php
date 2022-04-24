dce5d2f1cb8910af78583eaed539089737ed8aaa
Dharmik0121 <104291062+Dharmik0121@users.noreply.github.com>
2022-04-24 10:50:44 +0530

    my university website

---
+++ form-hendaler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_post['name'];
+$visitor_email = $_post['email'];
+$subject = $_post['subject'];
+$message = $_post['message'];
+
+$email_form = 'dharmikmeghani2195@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name : $name.\n".
+                "User Email :$visitor_email.\n".
+                "Subject : $subject.\n".
+                "User Message :$message.\n ";
+
+$to = 'meghanidharmik15.15@gmail.com';
+
+$headers = "Form : $email_form \r\n"
+
+$headers .="Reply-to:$visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location:contact.html")
+
+?>
